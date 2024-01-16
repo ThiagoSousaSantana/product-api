27b5df59e6c48ff2d2126d6d6ed74d6b2e4eb1f8
Thiago <thiago91098@gmail.com>
2024-01-15 21:39:22 -0300

    Project created

---
+++ src/test/java/com/tsantana/product/TestProductApplication.java
@@ -0,0 +1,23 @@
+package com.tsantana.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestProductApplication {
+
+	@Bean
+	@ServiceConnection
+	RabbitMQContainer rabbitContainer() {
+		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProductApplication::main).with(TestProductApplication.class).run(args);
+	}
+
+}
